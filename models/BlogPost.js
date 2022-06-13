8b58ffd78434b972332c955ffbaf958c1816b188
Geethika Singareddy <geethika1129@gmail.com>
2022-06-13 21:16:21 +0530

    add

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,6 +1,6 @@
 //mongoose
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true })
     //defining models
 const BlogPostSchema = new Schema({
     title: String,
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def ex(val):
+    val=val+'4'
+    val=val*2
+    return val
+ex("jump")
